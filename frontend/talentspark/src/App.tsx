3a07016e37f25b00c74a86de8c4520c5d8fd49ba
PRATHIBHARAGHU <prathibhar070@gmail.com>
2026-07-01 12:33:54 +0530

    Implementing Frontend CRUD

--- frontend/talentspark/src/App.tsx
+++ frontend/talentspark/src/App.tsx
@@ -1,64 +1,83 @@
-import Welcome from './components/Welcome';
-import Navbar from './components/NavBar';
-import CompanyCard from './components/CompanyCard';
-import JobCard from './components/JobCard';
-import Footer from './components/Footer';
-import {useEffect, useState } from 'react';
-import {getCompanies} from "./Services/CompanyService"
-import type {Company} from './types/company';
+// import Welcome from "./components/Welcome";
+import NavBar from "./components/NavBar";
+import CompanyCard from "./components/CompanyCard";
+import JobCard from "./components/JobCard";
+import Footer from "./components/Footer";
+import {useEffect,useState} from "react";
+import { getCompanies,updateCompany,deleteCompany,createCompany } from "./Services/CompanyService";
+import type {Company} from "./types/company"
 
 function App(){
-  const[loading, setLoading] = useState(true);
-  const[error, setError] = useState<Error | null>(null);
-  const[companies,setCompanies] = useState<Company[]>([]);
+  const [loading,setLoading] = useState(true);
+  const [error,setError] = useState<Error | null>(null)
+  const [companies,setCompanies] = useState<Company[]>([]);
 
-  async function fetchCompanies(){
+  async function fetchCompanies() {
     setLoading(true);
-    try{
+    try {
       const companies = await getCompanies();
       setCompanies(companies);
-    }catch(error){
+    } catch (error) {
       setError(error as Error);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
   }
-  useEffect(()=>{
+
+  async function handleEdit(company:Company){
+    try{
+      const updatedCompany = await updateCompany(company.id,company);
+      setCompanies(companies.map((company) => company.id === updatedCompany.id ? updatedCompany : company));
+    }catch(error){
+      setError(error as Error);
+    }
+  }
+
+  async function handleDelete(id:number){
+    try{
+      await deleteCompany(id);
+      setCompanies(companies.filter((company) => company.id !== id));
+    }catch(error){
+      setError(error as Error);
+    }
+  }
+
+  async function handleAdd(company:Company){
+    try{
+      const newCompany = await createCompany(company);
+      setCompanies([...companies,newCompany]);
+    }catch(error){
+      setError(error as Error);
+    }
+  }
+
+
+  useEffect(() => {
     fetchCompanies();
   }, []);
   
   if(loading){
-    return <div>Loading....</div>
+    return <div>Loading...</div>
   }
 
   if(error){
     return <div>Error: {error.message}</div>
   }
-
   
-  const handleEditCompany = (company: Company) => {
-    console.log('Edit company:', company);
-  };
-
-  const handleDeleteCompany = (id: number) => {
-    console.log('Delete company:', id);
-  };
-
-  const handleAddCompany = (company: Company) => {
-    console.log('Add company:', company);
-  };
-
   return(
-    <div>
-      <Navbar />
-      <Welcome />
-      <br />
-      <CompanyCard companies={companies} onedit={handleEditCompany} ondelete={handleDeleteCompany} onadd={handleAddCompany}/>
-      <br />
-      <JobCard />
-      <br />
-      <Footer />
-    </div>
+    <>
+    <NavBar />
+    {/* <Welcome /> */}
+    <br />
+    <CompanyCard 
+    companies={companies}
+    onedit={handleEdit}
+    ondelete={handleDelete}
+    onadd={handleAdd}
+    />
+    <JobCard />
+    <Footer />
+    </>
   )
 }
 
